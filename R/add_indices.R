f93d6b7f8024100f760d0886c7968871fbcd2c2d
Christian Zang <christian.zang@wzw.tum.de>
2019-06-14 15:42:50 +0200

    handle case when there is insufficient data for drought indices
    also: add dplyr progressbar

--- R/add_indices.R
+++ R/add_indices.R
@@ -21,27 +21,44 @@ add_indices <- function(x, scales = c(1, 2, 3, 6, 12), id = "id", lat = "lat", t
   n <- length(ids)
   n_scales <- length(scales)
   out <- list()
+  prog <- dplyr::progress_estimated(n)
   for (i in 1:n) {
+    prog$tick()$print()
     .x <- x[x[[id]] == ids[i], ]
-    .x$dmi <- round((.x[[prec]]/10)/(.x[[tmean]] + 10), 2)
-    .lat <- .x$lat[1]
-    ts_tmin <- stats::ts(.x[[tmin]], start = c(.x$year[1], .x$month[1]), frequency = 12)
-    ts_tmax <- stats::ts(.x[[tmax]], start = c(.x$year[1], .x$month[1]), frequency = 12)
-    ts_prec <- stats::ts(.x[[prec]], start = c(.x$year[1], .x$month[1]), frequency = 12)
-    ts_pet <- SPEI::hargreaves(ts_tmin, ts_tmax, Ra = NA, lat = .lat, Pre = ts_prec)
-    ts_cwb <- ts_prec - ts_pet
-    spei <- lapply(scales, function(x) round(SPEI::spei(ts_cwb, scale = x)$fitted, 2))
-    spi <- lapply(scales, function(x) round(SPEI::spi(ts_prec, scale = x)$fitted, 2))
-    for (j in 1:n_scales) {
-      spei_name <- paste0("spei", scales[j])
-      .x[[spei_name]] <- as.vector(spei[[j]])
-    }
-    for (j in 1:n_scales) {
-      spi_name <- paste0("spi", scales[j])
-      .x[[spi_name]] <- as.vector(spi[[j]])
+    if (!any(is.na(.x$prec)) & !any(is.na(.x$tmean)) &
+          !any(is.na(.x$tmin)) & !any(is.na(.x$tmax))) {
+      .x$dmi <- round((.x[[prec]]/10)/(.x[[tmean]] + 10), 2)
+      .lat <- .x$lat[1]
+      ts_tmin <- stats::ts(.x[[tmin]], start = c(.x$year[1], .x$month[1]), frequency = 12)
+      ts_tmax <- stats::ts(.x[[tmax]], start = c(.x$year[1], .x$month[1]), frequency = 12)
+      ts_prec <- stats::ts(.x[[prec]], start = c(.x$year[1], .x$month[1]), frequency = 12)
+      ts_pet <- SPEI::hargreaves(ts_tmin, ts_tmax, Ra = NA, lat = .lat, Pre = ts_prec)
+      ts_cwb <- ts_prec - ts_pet
+      spei <- lapply(scales, function(x) round(SPEI::spei(ts_cwb, scale = x)$fitted, 2))
+      spi <- lapply(scales, function(x) round(SPEI::spi(ts_prec, scale = x)$fitted, 2))
+      for (j in 1:n_scales) {
+        spei_name <- paste0("spei", scales[j])
+        .x[[spei_name]] <- as.vector(spei[[j]])
+      }
+      for (j in 1:n_scales) {
+        spi_name <- paste0("spi", scales[j])
+        .x[[spi_name]] <- as.vector(spi[[j]])
+      }
+    } else {
+      ## write NA dummy output
+      .x$dmi <- NA_real_
+      for (j in 1:n_scales) {
+        spei_name <- paste0("spei", scales[j])
+        .x[[spei_name]] <- NA_real_
+      }
+      for (j in 1:n_scales) {
+        spi_name <- paste0("spi", scales[j])
+        .x[[spi_name]] <- NA_real_
+      }
     }
     out[[i]] <- .x
   }
+  
   out <- Reduce(rbind, out)
   if (drop_lat) {
     out[[lat]] <- NULL
